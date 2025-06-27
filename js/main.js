16df67e047fc33ef6b08b229d4d8905e274670fa
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-06-27 13:03:15 +0200

    feat: implement obstacles

--- js/main.js
+++ js/main.js
@@ -26,7 +26,61 @@ class Player {
 }
 
 
-const player = new Player(); // create an instance of the class Player
+
+class Obstacle {
+    constructor(){
+        this.width = 20;
+        this.heigth = 10;
+        this.positionX = 30;
+        this.positionY = 100;
+        this.obstacleElm = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // step1: create the element
+        this.obstacleElm = document.createElement("div")
+
+        // step2: add content or modify (ex. innerHTML...)
+        this.obstacleElm.className = "obstacle"
+
+        //step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById("board")
+        parentElm.appendChild(this.obstacleElm)
+    }
+    updateUI(){
+        this.obstacleElm.style.left = this.positionX + "vw"
+        this.obstacleElm.style.bottom = this.positionY + "vh"
+        this.obstacleElm.style.width = this.width + "vw"
+        this.obstacleElm.style.height = this.heigth + "vh"
+    }
+    moveDown(){
+        this.positionY--;
+        this.updateUI();
+    }
+}
+
+
+const player = new Player();
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle)
+}, 3000);
+
+
+// move obstacles
+setInterval(() => {
+    obstaclesArr.forEach((obstacleInstance, i, arr) => {
+        obstacleInstance.moveDown();
+    })
+}, 40)
+
+
+
 
 // Event listeners
 document.addEventListener('keydown', (e) => {
